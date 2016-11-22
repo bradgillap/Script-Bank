2b3497e9eb381a08e34c60e0093650cfdb07a8cc
Bradley Gillap <bradgillap@gmail.com>
2016-11-22 01:50:40 -0500

    more info

--- powershell/Base Examples/EnvironmentalVariables.ps1
+++ powershell/Base Examples/EnvironmentalVariables.ps1
@@ -8,3 +8,4 @@ Get-Item Env:Path #Show environmental variables path settings in Windows.
 [Environment]::GetFolderPath("Desktop") #Desktop path example.
 
 [Environment+SpecialFolder]::GetNames([Environment+SpecialFolder]) #All of the available special folder location paths
+
