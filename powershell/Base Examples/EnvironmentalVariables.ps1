7b0bd43dc021dda1cfc63b0066d7b40e6baec505
Bradley Gillap <bradgillap@gmail.com>
2016-11-22 01:45:17 -0500

    Special folders!

--- powershell/Base Examples/EnvironmentalVariables.ps1
+++ powershell/Base Examples/EnvironmentalVariables.ps1
@@ -1,3 +1,10 @@
-﻿#Environmental Variables
+﻿
 
-$host
+Get-ChildItem Env: #Environmental Variables for the system.
+
+Get-Item Env:Path #Show environmental variables path settings in Windows.
+
+
+[Environment]::GetFolderPath("Desktop") #Desktop path example.
+
+[Environment+SpecialFolder]::GetNames([Environment+SpecialFolder]) #All of the available special folder location paths
