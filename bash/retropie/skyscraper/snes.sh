96f31993eb80e90e55cd0e02beb343daa58df964
Bradley Gillap <bradgillap@gmail.com>
2017-12-04 15:50:20 -0500

    Update snes.sh

--- bash/retropie/skyscraper/snes.sh
+++ bash/retropie/skyscraper/snes.sh
@@ -1,42 +1,57 @@
 #!/bin/bash
+
+#Set options
+
+CONSOLE = "snes"
+
+USR1 = "Brad"
+USR2 = "Ethan"
+USR3 = "Haley"
+USR4 = "Mirm"
+
+SCRAPER1 = "screenscraper "
+SCRAPER2 = "openretro"
+SCRAPER3 = "thegamesdb"
+SCRAPER4 = "worldofspectrum"
+
 #Update master first.
 
-Skyscraper -p snes -i ~/RetroPie/roms/snes/Master --unattend -s screenscraper -o ~/RetroPie/roms/snes/images -v ~/RetroPie/roms/snes/video
-Skyscraper -p snes -i ~/RetroPie/roms/snes/Master --unattend -s openretro -o ~/RetroPie/roms/snes/images -v ~/RetroPie/roms/snes/video
-Skyscraper -p snes -i ~/RetroPie/roms/snes/Master --unattend -s thegamesdb -o ~/RetroPie/roms/snes/images -v ~/RetroPie/roms/snes/video
-Skyscraper -p snes -i ~/RetroPie/roms/snes/Master --unattend -s worldofspectrum -o ~/RetroPie/roms/snes/images -v ~/RetroPie/roms/snes/video
+Skyscraper -p snes -i ~/RetroPie/roms/$CONSOLE/Master --unattend -s $SCRAPER1 -o ~/RetroPie/roms/$CONSOLE/images -v ~/RetroPie/roms/$CONSOLE/video
+Skyscraper -p snes -i ~/RetroPie/roms/$CONSOLE/Master --unattend -s $SCRAPER2 -o ~/RetroPie/roms/$CONSOLE/images -v ~/RetroPie/roms/$CONSOLE/video
+Skyscraper -p snes -i ~/RetroPie/roms/$CONSOLE/Master --unattend -s $SCRAPER3 -o ~/RetroPie/roms/$CONSOLE/images -v ~/RetroPie/roms/$CONSOLE/video
+Skyscraper -p snes -i ~/RetroPie/roms/$CONSOLE/Master --unattend -s $SCRAPER4 -o ~/RetroPie/roms/$CONSOLE/images -v ~/RetroPie/roms/$CONSOLE/video
 
 #Update videos per user
 #Brad
-Skyscraper -p snes -i ~/RetroPie/roms/snes/Brad --unattend -s screenscraper --videos -o ~/RetroPie/roms/snes/images -v ~/RetroPie/roms/snes/video
-Skyscraper -p snes -i ~/RetroPie/roms/snes/Brad --unattend -s openretro --videos -o ~/RetroPie/roms/snes/images -v ~/RetroPie/roms/snes/video
-Skyscraper -p snes -i ~/RetroPie/roms/snes/Brad --unattend -s thegamesdb --videos -o ~/RetroPie/roms/snes/images -v ~/RetroPie/roms/snes/video
-Skyscraper -p snes -i ~/RetroPie/roms/snes/Brad --unattend -s worldofspectrum --videos -o ~/RetroPie/roms/snes/images -v ~/RetroPie/roms/snes/video
-Skyscraper -p snes -i ~/RetroPie/roms/snes/Brad --unattend --videos -s localdb -o ~/RetroPie/roms/snes/images -v ~/RetroPie/roms/snes/video
+Skyscraper -p snes -i ~/RetroPie/roms/$CONSOLE/$USR1 --unattend -s $SCRAPER1 --videos -o ~/RetroPie/roms/$CONSOLE/images -v ~/RetroPie/roms/$CONSOLE/video
+Skyscraper -p snes -i ~/RetroPie/roms/$CONSOLE/$USR1 --unattend -s $SCRAPER2 --videos -o ~/RetroPie/roms/$CONSOLE/images -v ~/RetroPie/roms/$CONSOLE/video
+Skyscraper -p snes -i ~/RetroPie/roms/$CONSOLE/$USR1 --unattend -s $SCRAPER3 --videos -o ~/RetroPie/roms/$CONSOLE/images -v ~/RetroPie/roms/$CONSOLE/video
+Skyscraper -p snes -i ~/RetroPie/roms/$CONSOLE/$USR1 --unattend -s $SCRAPER4 --videos -o ~/RetroPie/roms/$CONSOLE/images -v ~/RetroPie/roms/$CONSOLE/video
+Skyscraper -p snes -i ~/RetroPie/roms/$CONSOLE/$USR1 --unattend --videos -s localdb -o ~/RetroPie/roms/$CONSOLE/images -v ~/RetroPie/roms/$CONSOLE/video
 
 #Ethan
-Skyscraper -p snes -i ~/RetroPie/roms/snes/Ethan --unattend -s screenscraper --videos -o ~/RetroPie/roms/snes/images -v ~/RetroPie/roms/snes/video
-Skyscraper -p snes -i ~/RetroPie/roms/snes/Ethan --unattend -s openretro --videos -o ~/RetroPie/roms/snes/images -v ~/RetroPie/roms/snes/video
-Skyscraper -p snes -i ~/RetroPie/roms/snes/Ethan --unattend -s thegamesdb --videos -o ~/RetroPie/roms/snes/images -v ~/RetroPie/roms/snes/video
-Skyscraper -p snes -i ~/RetroPie/roms/snes/Ethan --unattend -s worldofspectrum --videos -o ~/RetroPie/roms/snes/images -v ~/RetroPie/roms/snes/video
-Skyscraper -p snes -i ~/RetroPie/roms/snes/Ethan --unattend --videos -s localdb -o ~/RetroPie/roms/snes/images -v ~/RetroPie/roms/snes/video
+Skyscraper -p snes -i ~/RetroPie/roms/$CONSOLE/$USR2 --unattend -s $SCRAPER1 --videos -o ~/RetroPie/roms/$CONSOLE/images -v ~/RetroPie/roms/$CONSOLE/video
+Skyscraper -p snes -i ~/RetroPie/roms/$CONSOLE/$USR2 --unattend -s $SCRAPER2 --videos -o ~/RetroPie/roms/$CONSOLE/images -v ~/RetroPie/roms/$CONSOLE/video
+Skyscraper -p snes -i ~/RetroPie/roms/$CONSOLE/$USR2 --unattend -s $SCRAPER3 --videos -o ~/RetroPie/roms/$CONSOLE/images -v ~/RetroPie/roms/$CONSOLE/video
+Skyscraper -p snes -i ~/RetroPie/roms/$CONSOLE/$USR2 --unattend -s $SCRAPER4 --videos -o ~/RetroPie/roms/$CONSOLE/images -v ~/RetroPie/roms/$CONSOLE/video
+Skyscraper -p snes -i ~/RetroPie/roms/$CONSOLE/$USR2 --unattend --videos -s localdb -o ~/RetroPie/roms/$CONSOLE/images -v ~/RetroPie/roms/$CONSOLE/video
 
 #Haley
-Skyscraper -p snes -i ~/RetroPie/roms/snes/Haley --unattend -s screenscraper --videos -o ~/RetroPie/roms/snes/images -v ~/RetroPie/roms/snes/video
-Skyscraper -p snes -i ~/RetroPie/roms/snes/Haley --unattend -s openretro --videos -o ~/RetroPie/roms/snes/images -v ~/RetroPie/roms/snes/video
-Skyscraper -p snes -i ~/RetroPie/roms/snes/Haley --unattend -s thegamesdb --videos -o ~/RetroPie/roms/snes/images -v ~/RetroPie/roms/snes/video
-Skyscraper -p snes -i ~/RetroPie/roms/snes/Haley --unattend -s worldofspectrum --videos -o ~/RetroPie/roms/snes/images -v ~/RetroPie/roms/snes/video
-Skyscraper -p snes -i ~/RetroPie/roms/snes/Haley --unattend --videos -s localdb -o ~/RetroPie/roms/snes/images -v ~/RetroPie/roms/snes/video
+Skyscraper -p snes -i ~/RetroPie/roms/$CONSOLE/$USR3 --unattend -s $SCRAPER1 --videos -o ~/RetroPie/roms/$CONSOLE/images -v ~/RetroPie/roms/$CONSOLE/video
+Skyscraper -p snes -i ~/RetroPie/roms/$CONSOLE/$USR3 --unattend -s $SCRAPER2 --videos -o ~/RetroPie/roms/$CONSOLE/images -v ~/RetroPie/roms/$CONSOLE/video
+Skyscraper -p snes -i ~/RetroPie/roms/$CONSOLE/$USR3 --unattend -s $SCRAPER3 --videos -o ~/RetroPie/roms/$CONSOLE/images -v ~/RetroPie/roms/$CONSOLE/video
+Skyscraper -p snes -i ~/RetroPie/roms/$CONSOLE/$USR3 --unattend -s $SCRAPER4 --videos -o ~/RetroPie/roms/$CONSOLE/images -v ~/RetroPie/roms/$CONSOLE/video
+Skyscraper -p snes -i ~/RetroPie/roms/$CONSOLE/$USR3 --unattend --videos -s localdb -o ~/RetroPie/roms/$CONSOLE/images -v ~/RetroPie/roms/$CONSOLE/video
 
 #Mirm
-Skyscraper -p snes -i ~/RetroPie/roms/snes/Mirm --unattend -s screenscraper --videos -o ~/RetroPie/roms/snes/images -v ~/RetroPie/roms/snes/video
-Skyscraper -p snes -i ~/RetroPie/roms/snes/Mirm --unattend -s openretro --videos -o ~/RetroPie/roms/snes/images -v ~/RetroPie/roms/snes/video
-Skyscraper -p snes -i ~/RetroPie/roms/snes/Mirm --unattend -s thegamesdb --videos -o ~/RetroPie/roms/snes/images -v ~/RetroPie/roms/snes/video
-Skyscraper -p snes -i ~/RetroPie/roms/snes/Mirm --unattend -s worldofspectrum --videos -o ~/RetroPie/roms/snes/images -v ~/RetroPie/roms/snes/video
-Skyscraper -p snes -i ~/RetroPie/roms/snes/Mirm --unattend --videos -s localdb -o ~/RetroPie/roms/snes/images -v ~/RetroPie/roms/snes/video
+Skyscraper -p snes -i ~/RetroPie/roms/$CONSOLE/$USR4 --unattend -s $SCRAPER1 --videos -o ~/RetroPie/roms/$CONSOLE/images -v ~/RetroPie/roms/$CONSOLE/video
+Skyscraper -p snes -i ~/RetroPie/roms/$CONSOLE/$USR4 --unattend -s $SCRAPER2 --videos -o ~/RetroPie/roms/$CONSOLE/images -v ~/RetroPie/roms/$CONSOLE/video
+Skyscraper -p snes -i ~/RetroPie/roms/$CONSOLE/$USR4 --unattend -s $SCRAPER3 --videos -o ~/RetroPie/roms/$CONSOLE/images -v ~/RetroPie/roms/$CONSOLE/video
+Skyscraper -p snes -i ~/RetroPie/roms/$CONSOLE/$USR4 --unattend -s $SCRAPER4 --videos -o ~/RetroPie/roms/$CONSOLE/images -v ~/RetroPie/roms/$CONSOLE/video
+Skyscraper -p snes -i ~/RetroPie/roms/$CONSOLE/$USR4 --unattend --videos -s localdb -o ~/RetroPie/roms/$CONSOLE/images -v ~/RetroPie/roms/$CONSOLE/video
 
 #After users are updated add videos back to master from the local db.
 
-Skyscraper -p snes -i ~/RetroPie/roms/snes/Master --unattend --videos -s localdb -o ~/RetroPie/roms/snes/images -v ~/RetroPie/roms/snes/video
+Skyscraper -p snes -i ~/RetroPie/roms/$CONSOLE/Master --unattend --videos -s localdb -o ~/RetroPie/roms/$CONSOLE/images -v ~/RetroPie/roms/$CONSOLE/video
 
 #Combine gamelist.xml files for users and master.  Place in the consoles directory.
