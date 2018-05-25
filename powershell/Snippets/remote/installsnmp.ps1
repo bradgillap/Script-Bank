a331b4dc2f85eb1daa39954a936dc10d248400b4
Bradley Gillap <bradgillap@gmail.com>
2018-05-25 13:18:07 -0400

    Update installsnmp.ps1

--- powershell/Snippets/remote/installsnmp.ps1
+++ powershell/Snippets/remote/installsnmp.ps1
@@ -1,4 +1,4 @@
 $computers = get-content D:\complist.txt
 
-foreach($computer in $computers){
+foreach($computer in $computers)
     {Invoke-Command -computername $computer -ScriptBlock {Enable-WindowsOptionalFeature -online -FeatureName snmp; Enable-WindowsOptionalFeature -online -FeatureName wmisnmpprovider}};
