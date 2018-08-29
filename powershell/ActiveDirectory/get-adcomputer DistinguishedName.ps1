43f7f1d7892150b252d935abf0bcc1ea8dbd850e
Bradley Gillap <bradgillap@gmail.com>
2018-08-29 13:16:52 -0400

    Update get-adcomputer DistinguishedName.ps1

--- powershell/ActiveDirectory/get-adcomputer DistinguishedName.ps1
+++ powershell/ActiveDirectory/get-adcomputer DistinguishedName.ps1
@@ -1,6 +1,6 @@
 get-adcomputer "COMPUTERNAME" -properties "DistinguishedName" | select -Property DistinguishedName
 //Strip title and crush linespacing
-//get-adcomputer "vic-st-01" -properties "DistinguishedName" | select -ExpandProperty DistinguishedName | Out-File -FilePath "D:\DistinguishedNames.txt" -Append -NoClobber
+get-adcomputer "vic-st-01" -properties "DistinguishedName" | select -ExpandProperty DistinguishedName | Out-File -FilePath "D:\DistinguishedNames.txt" -Append -NoClobber
 
 // with tables and spacing
-//get-adcomputer "vic-st-01" -properties "DistinguishedName" | select -Property DistinguishedName | format-table -HideTableHeaders | Out-File -FilePath "D:\DistinguishedNames.txt" -Append -NoClobber
+get-adcomputer "vic-st-01" -properties "DistinguishedName" | select -Property DistinguishedName | format-table -HideTableHeaders | Out-File -FilePath "D:\DistinguishedNames.txt" -Append -NoClobber
