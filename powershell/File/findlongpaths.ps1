676ba83f76f3f58a84d917f92f82ddb1f6fa8939
Bradley Gillap <bradgillap@gmail.com>
2018-09-02 05:38:40 -0400

    Update findlongpaths.ps1

--- powershell/File/findlongpaths.ps1
+++ powershell/File/findlongpaths.ps1
@@ -1,3 +1,3 @@
 //Finds paths of long file names greater than X. Good for fixing folder redirection issues.
 
-Out-File U:\longfilepath.txt ; cmd "U:\" "dir /b /s /a" | ForEach-Object { if ($_.length -gt 230) {$_ | Out-File -append longfilepath.txt}}
+cmd /c dir /s /b |? {$_.length -gt 260} > longpath.txt
