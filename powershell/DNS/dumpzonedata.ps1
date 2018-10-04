92d0ceb0960874df2a15daed425d3d7142badeaf
Bradley Gillap <bradgillap@gmail.com>
2018-10-04 12:06:23 -0400

    Create dumpzonedata.ps1
    Import into excel for tab delim info. Super easy to manipulate.

---
+++ powershell/DNS/dumpzonedata.ps1
@@ -0,0 +1,7 @@
+$DNSServer = "servernameOrIp"
+$Zones = @(Get-DnsServerZone -ComputerName $DNSServer)
+ForEach ($Zone in $Zones) {
+	Write-Host "`n$($Zone.ZoneName)" -ForegroundColor "Green"
+	$Results = $Zone | Get-DnsServerResourceRecord -ComputerName $DNSServer
+	echo $Results > "$($Zone.ZoneName).txt"
+}
