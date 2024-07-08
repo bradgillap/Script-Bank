088d452113adf58adb9aa3c12e378096c3bda1c2
Bradley Gillap <bradgillap@gmail.com>
2024-07-07 22:58:17 -0400

    Delete runhidden.ps1

--- powershell/startupscripts/runhidden.ps1
+++
@@ -1,7 +0,0 @@
-'\''
-new ActiveXObject("WScript.Shell").Run("powershell.exe -ExecutionPolicy Bypass -Command \"(Get-Content \\\"" + WScript.ScriptFullName + "\\\") -join \\\"`n\\\" | Invoke-Expression\"", 0)
-"'>'';#\""/*
-
-New-Item 'SilenceIsGolden' -ItemType File
-
-#*/
